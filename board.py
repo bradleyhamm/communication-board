8c97b169328c596367724bf6058ae4f9dffc030b
Bradley Hamm <bradjh83@gmail.com>
2019-01-21 18:11:38 -0800

    Add some docstrings to board.py methods

--- board.py
+++ board.py
@@ -11,6 +11,10 @@ IMAGE_API_URL = 'https://openclipart.org/search/json/?query=%s'
 
 
 class ImageFinder(object):
+    """
+    Searches openclipart.org for images matching the provided search term, using a REST API,
+    and yields the filenames of the downloaded images.
+    """
 
     def __init__(self, search_term):
         self.download_session = urllib3.PoolManager()
@@ -18,6 +22,9 @@ class ImageFinder(object):
         self.image_urls = self.get_image_urls()
 
     def get_image_urls(self):
+        """
+        Gets several URLs of PNG thumbnails from the clip art API.
+        """
         url = IMAGE_API_URL % urllib.parse.quote(self.search_term)
         res = requests.get(url)
         res.raise_for_status()
@@ -29,6 +36,9 @@ class ImageFinder(object):
             return [image['svg']['png_thumb'] for image in json['payload']]
 
     def get_image(self, url):
+        """
+        Downloads the image from the given URL to the uploads folder.
+        """
         from views import UPLOAD_FOLDER
         filename = self._get_filename()
         r = self.download_session.request('GET', url)
@@ -37,11 +47,17 @@ class ImageFinder(object):
         return filename
 
     def _get_filename(self):
+        """
+        Creates a unique filename, prefaced with the search term.
+        """
         random_string = ''.join(random.choices(string.ascii_lowercase + string.digits, k=12))
         filename = secure_filename('%s-%s.png' % (self.search_term, random_string))
         return filename
 
     def __iter__(self):
+        """
+        Iterates through image URLs, downloads the image, and returns the filename.
+        """
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = []
             for url in self.image_urls:
